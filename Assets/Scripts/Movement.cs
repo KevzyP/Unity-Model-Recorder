8497d0f1ba678b00d18a17aa1864b538ebe3bbca
DESKTOP-TMGCSDT\prime <keytotruth12@gmail.com>
2021-04-29 11:33:17 +0700

    with animancer

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,44 +1,76 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.XR.Interaction.Toolkit;
+//using UnityEngine.InputSystem;
+//using UnityEditor.Recorder;
 
-public class Movement : MonoBehaviour
-{
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    public float playerSpeed = 2.0f;
-    public float jumpHeight = 1.0f;
-    public float gravityValue = -9.81f;
+//public class ActivateRecording : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    [SerializeField] InputActionAsset actionAsset;
+//    [SerializeField] string controllerName;
+//    [SerializeField] string PrimaryButton;
+//    [SerializeField] string SecondaryButton;
+//    [SerializeField] string TriggerButton;
 
-    private void Start()
-    {
-        controller = gameObject.AddComponent<CharacterController>();
-    }
+//    private InputActionMap m_actionMap;
+//    private InputAction m_inputActionPrimaryButton;
+//    private InputAction m_inputActionSecondaryButton;
+//    private InputAction m_inputActionTrigger;
 
-    void Update()
-    {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+//    // bool activated;
+//    void Awake()
+//    {
+//        //get actions
+//        m_actionMap = actionAsset.FindActionMap(controllerName);
+//        m_inputActionPrimaryButton = m_actionMap.FindAction(PrimaryButton);
+//        m_inputActionSecondaryButton = m_actionMap.FindAction(SecondaryButton);
+//        m_inputActionTrigger = m_actionMap.FindAction(TriggerButton);
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
 
-        // Changes the height position of the player..
-        //if (Input.GetButtonDown("Jump") && groundedPlayer)
-        //{
-        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        //}
+//    }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
-}
+//    private void OnEnable()
+//    {
+//        m_inputActionPrimaryButton.Enable();
+//        m_inputActionSecondaryButton.Enable();
+//        m_inputActionTrigger.Enable();
+//        //  GameObject.Find("Recorder").GetComponent<Recorder_Controller>().Prepare();
+//        //activated = true;
+//    }
+
+//    private void OnDisable()
+//    {
+//        m_inputActionPrimaryButton.Disable();
+//        m_inputActionSecondaryButton.Disable();
+//        m_inputActionTrigger.Disable();
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        var PrimaryButtonPress = m_inputActionPrimaryButton.ReadValue<float>();
+//        var SecondaryButtonPress = m_inputActionSecondaryButton.ReadValue<float>();
+//        var triggerValue = m_inputActionTrigger.ReadValue<float>();
+
+//        if (triggerValue == 1)
+//        {
+//            GameObject.Find("Recorder").GetComponent<Recorder_Controller>().Record();
+//            //PrimaryButtonPress = 0;
+//            //activated = true;
+//        }
+//        else if (SecondaryButtonPress == 1)
+//        {
+//            GameObject.Find("Recorder").GetComponent<Recorder_Controller>().stopRecord();
+
+//            SecondaryButtonPress = 0;
+//            //GameObject.Find("Recorder").GetComponent<Recorder_Controller>().Prepare();
+//            //activated = false;
+//        }
+
+//    }
+
+
+//}
--- Assets/Scripts/Recorder_Controller.cs
+++ Assets/Scripts/Recorder_Controller.cs
@@ -3,32 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Recorder;
 using System.IO;
+using System;
 
 public class Recorder_Controller : MonoBehaviour
 {
     
     RecorderController m_RecorderController;
     public GameObject ObjectToRecord;
+
+    //public Animator dummy_playback;
     //public float timer = 0;
     // Start is called before the first frame update
     void OnEnable()
     {
         Setup();
+        
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)) 
-        {
-            Record();
+        //if (Input.GetKeyDown(KeyCode.R)) 
+        //{
+        //    Record();
 
-        }
-        if(Input.GetKeyUp(KeyCode.R))
-        {
-            stopRecord();
-        }
+        //}
+        //if(Input.GetKeyUp(KeyCode.R))
+        //{
+        //    stopRecord();
+        //}
     }
     
     void Setup()
@@ -46,19 +50,32 @@ public class Recorder_Controller : MonoBehaviour
         animationRecorder.AnimationInputSettings.AddComponentToRecord(typeof(Transform));
 
         //setup output
-        animationRecorder.OutputFile = "D:\\Yes\\Unity Recording\\Assets\\Animations\\duplo";
+        animationRecorder.OutputFile = "D:\\Yes\\Unity Recorder Experiment\\Assets\\Animations\\animResult";
         controllerSettings.AddRecorderSettings(animationRecorder);
         RecorderOptions.VerboseMode = true;
     }
     
-    void Record()
+       
+    public void Record()
     {
+        //dummy_playback.enabled = false;
         m_RecorderController.PrepareRecording();
         m_RecorderController.StartRecording();
     }
 
-    void stopRecord()
+    public void stopRecord()
     {
         m_RecorderController.StopRecording();
+        //StartCoroutine(CR_TestPlayback());
     }
+
+    //private IEnumerator CR_TestPlayback()
+    //{
+    //    Debug.Log("Wait Start");
+    //    yield return new WaitForSeconds(5f);
+    //    Debug.Log("Wait Ready");
+    //    Debug.Log("Playback");
+        
+
+    //}
 }
